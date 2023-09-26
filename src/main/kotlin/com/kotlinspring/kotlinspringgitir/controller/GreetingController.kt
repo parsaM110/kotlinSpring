184d0c5a005284194e577bd2b4297539edd0b206
parsaM110 <pmomeni80@gmail.com>
2023-09-27 00:48:50 +0330

    'logging config and writing an info'

--- src/main/kotlin/com/kotlinspring/kotlinspringgitir/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinspring/kotlinspringgitir/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.kotlinspring.kotlinspringgitir.controller
 
 import com.kotlinspring.kotlinspringgitir.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/greetings")
 class GreetingController(val greetingsService: GreetingsService) {
+
+    companion object : KLogging()
+
     @GetMapping("/{name}")
     fun  retrieveGreeting(@PathVariable("name") name:String) : String{
 //        return "Hello $name"
+        logger.info("Name is $name")
         return greetingsService.retrieveGreeting(name)
     }
 }
